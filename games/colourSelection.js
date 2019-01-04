9435dc199740b0bebfaabaadd63c1382e7d484ee
akankshka <44019583+akankshakutal@users.noreply.github.com>
2019-01-04 21:32:42 +0530

    Change colours to pokemon

--- games/colourSelection.js
+++ games/colourSelection.js
@@ -1,24 +1,20 @@
-const colors = ["red", "blue", "green", "yellow"];
+let pokemons = ["pikachu", "meowth", "bulbasaur", "togepi"];
 let score = 10;
-let colorNameIndex = 1;
+let pokemonNameIndex = 1;
 
-const changeColorName = function(box) {
-  updateScore(box.id, colors[colorNameIndex]);
-  if (score < 0) {
-    document.getElementById("main").innerHTML =
-      "<h1 style = 'margin-top:10%; color:red'> GAME OVER......! </h1>";
+const changePokemonName = function(box) {
+  updateScore(box.id, pokemons[pokemonNameIndex]);
+  if (score < -10) {
+    document.getElementById("main").innerHTML = "<h1>GAME OVER ....!</h1>";
   }
-  colorNameIndex = Math.floor(Math.random() * 4);
-  const colorIndex = Math.floor(Math.random() * 4);
-
-  document.getElementById("heading").innerText = colors[
-    colorNameIndex
+  pokemonNameIndex = Math.floor(Math.random() * 4);
+  document.getElementById("heading").innerText = pokemons[
+    pokemonNameIndex
   ].toUpperCase();
-  document.getElementById("heading").style.color = colors[colorIndex];
 };
 
-const updateScore = function(id, color) {
-  if (id == color) {
+const updateScore = function(id, pokemon) {
+  if (id == pokemon) {
     score = score + 10;
     document.getElementById("score").innerText = score;
   } else {
